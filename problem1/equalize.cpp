3669c114e43413b19c052f6643d3ba8ce943b776
Yuan Xia <yuan.xia.cpp@gmail.com>
2014-02-19 00:22:50 -0500

    finish compile debug

--- problem1/equalize.cpp
+++ problem1/equalize.cpp
@@ -12,11 +12,10 @@ int w = 0;
 int h = 0;
 
 
-unsigned char* readInTiff(char* filename);
 void grayscale(unsigned char* im_in, unsigned char* im_gray);
-void hist(unsigned char* im_gray, int* hist);
-void cdf(int *hist, double* cdf);
-void equalized(unsigned char* im_gray, double* cdf, unsigned char* equalized_im);
+void hist(unsigned char* im_gray, int* im_hist);
+void cdf(int *im_hist, double* im_cdf);
+void equalized(unsigned char* im_gray, double* im_cdf, unsigned char* equalized_im);
 void equalized(unsigned char* im_in, unsigned char* equalized_im, unsigned char* final_out);
 
 
@@ -26,18 +25,25 @@ int main(int argc, char* argv[]) {
 	return 1;
   }
 
-  unsigned char* im_in = readInTiff(argv[1]);
+
+  unsigned char* im_in = new unsigned char[max_w*max_h*3];
+  
+  if (!load_tiff(argv[1], im_in, w, h)) {
+	cout << "Could not read input file: " << argv[1] << endl;
+	return 1;
+  }
+
   unsigned char* im_gray = new unsigned char[w*h];
   grayscale(im_in, im_gray);
 
-  int* hist = new int[256];
-  hist(im_gray, hist);
+  int* im_hist = new int[256];
+  hist(im_gray, im_hist);
 
-  double* cdf = new double[256];
-  cdf(hist, cdf);
+  double* im_cdf = new double[256];
+  cdf(im_hist, im_cdf);
   
   unsigned char* equalized_im = new unsigned char[w*h];
-  equalized(im_gray, cdf, equalized_im);
+  equalized(im_gray, im_cdf, equalized_im);
 
   unsigned char* final_out = new unsigned char[w*h*3];
   equalized(im_in, equalized_im, final_out);
@@ -50,16 +56,6 @@ int main(int argc, char* argv[]) {
 
 }
 
-unsigned char* readInTiff(char* filename) {
-  unsigned char* im_in = new unsigned char[max_w*max_h*3];
-
-  if (!load_tiff(filename, im_in, w, h)) {
-	cout << "Could not read input file" << filename << endl;
-	return 1;
-  }
-  return image;
-}
-
 void grayscale(unsigned char* im_in, unsigned char* im_gray) {
   for (int j = 0; j < h; j++) {
 	for (int i = 0; i < w; i++) {
@@ -70,34 +66,34 @@ void grayscale(unsigned char* im_in, unsigned char* im_gray) {
   }
   return;
 }
-void hist(unsigned char* im_gray, int* hist) {
+void hist(unsigned char* im_gray, int* im_hist) {
   for (int i = 0; i < 256; i++)
-	hist[i] = 0;
+	im_hist[i] = 0;
 
   for (int j = 0; j < h; j++)
 	for (int i = 0; i < w; i++) {
-	  hist[im_gray[j*w+i]]++;
+	  im_hist[im_gray[j*w+i]]++;
 	}
   return;
 }
-void cdf(int* hist, double* cdf) {
+void cdf(int* im_hist, double* im_cdf) {
   double total = 0.0;
   for (int i = 0; i < 256; i++)
-	total += hist[i];
+	total += im_hist[i];
   for (int i = 0; i < 256; i++)
-	cdf[i] = (double)(hist[i]) / total;
+	im_cdf[i] = (double)(im_hist[i]) / total;
   return;
 }
-void equalized(unsigned char* im_gray, double* cdf, unsigned char* equalized_im) {
+void equalized(unsigned char* im_gray, double* im_cdf, unsigned char* equalized_im) {
   double min_cdf = 0.0;
   for (int i = 0; i < 256; i++)
-	if (cdf[i] < min_cdf)
-	  min_cdf = cdf[i];
+	if (im_cdf[i] < min_cdf)
+	  min_cdf = im_cdf[i];
 
 
   for (int j = 0; j < h; j++)
 	for (int i = 0; i < w; i++) {
-	  equalized_im[j*w+i] = round((cdf[im_gray[j*w+i]]-min_cdf) / (1.0-min_cdf) * 255);
+	  equalized_im[j*w+i] = round((im_cdf[im_gray[j*w+i]]-min_cdf) / (1.0-min_cdf) * 255);
 	}
 
   return;
@@ -108,7 +104,7 @@ void equalized(unsigned char* im_in, unsigned char* equalized_im, unsigned char*
 	for (int i = 0; i < w; i++) {
 	  int index = 3*(j*w+i);
 	  double gray_value = im_in[index]*0.3 + im_in[index+1]*0.5 + im_in[index+2]*0.2;
-	  double ratio = gray_value / equalized_im[y*w+i];
+	  double ratio = gray_value / equalized_im[j*w+i];
 	  final_out[index] = im_in[index] * ratio;
 	  final_out[index+1] = im_in[index+1] * ratio;
 	  final_out[index+2] = im_in[index+2] * ratio;
