09c27efe1501049139bdb7fc4d41654d96904aea
Yuan Xia <yuan.xia@yale.edu>
2014-02-19 20:09:35 -0500

    finish debugging problem1

--- problem1/equalize.cpp
+++ problem1/equalize.cpp
@@ -37,6 +37,7 @@ int main(int argc, char* argv[]) {
 	return 1;
   }
 
+
   unsigned char* im_gray = new unsigned char[w*h];
   grayscale(im_in, im_gray);
 
@@ -45,6 +46,7 @@ int main(int argc, char* argv[]) {
 
   double* im_cdf = new double[256];
   cdf(im_hist, im_cdf);
+
   
   unsigned char* equalized_im = new unsigned char[w*h];
   equalized(im_gray, im_cdf, equalized_im);
@@ -73,7 +75,7 @@ void grayscale(unsigned char* im_in, unsigned char* im_gray) {
 void hist(unsigned char* im_gray, int* im_hist) {
   for (int i = 0; i < 256; i++)
 	im_hist[i] = 0;
-
+  
   for (int j = 0; j < h; j++)
 	for (int i = 0; i < w; i++) {
 	  im_hist[im_gray[j*w+i]]++;
@@ -84,14 +86,19 @@ void cdf(int* im_hist, double* im_cdf) {
   double total = 0.0;
   for (int i = 0; i < 256; i++)
 	total += im_hist[i];
-  for (int i = 0; i < 256; i++)
-	im_cdf[i] = (double)(im_hist[i]) / total;
+
+  //cout << "Total = " << total << endl;
+  
+
+  double up = 0.0;
+  for (int i = 0; i < 256; i++) {
+	up += im_hist[i];
+	im_cdf[i] = up / total;
+  }
   return;
 }
 void equalized(unsigned char* im_gray, double* im_cdf, unsigned char* equalized_im) {
-  double min_cdf = 0.0;
-  for (int i = 0; i < 256; i++)
-	min_cdf = min(min_cdf, im_cdf[i]);
+  double min_cdf = im_cdf[0];
 
   for (int j = 0; j < h; j++)
 	for (int i = 0; i < w; i++) {
@@ -106,10 +113,14 @@ void equalized(unsigned char* im_in, unsigned char* equalized_im, unsigned char*
 	for (int i = 0; i < w; i++) {
 	  int index = 3*(j*w+i);
 	  double gray_value = im_in[index]*0.3 + im_in[index+1]*0.5 + im_in[index+2]*0.2;
-	  double ratio = gray_value / equalized_im[j*w+i];
-	  final_out[index] = im_in[index] * ratio;
-	  final_out[index+1] = im_in[index+1] * ratio;
-	  final_out[index+2] = im_in[index+2] * ratio;
+	  double ratio =  equalized_im[j*w+i] / gray_value;
+
+	  int r = im_in[index] * ratio;
+	  int g = im_in[index+1] * ratio;
+	  int b = im_in[index+2] * ratio;
+	  final_out[index] = (r > 255) ? 255 : r;
+	  final_out[index+1] = (g > 255) ? 255 : g;
+	  final_out[index+2] = (b > 255) ? 255 : b;
 	}
   return;
 
