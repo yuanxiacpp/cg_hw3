db0172f5ec1bd524ace417b0a7d5f10e2a4497ab
Yuan Xia <yuan.xia@yale.edu>
2014-02-20 16:59:56 -0500

    calculate normal on the fly which speed up the process a lot(wtf), guess matlab has serious mem accessing issue

--- problem4/view_normals_image.m
+++ problem4/view_normals_image.m
@@ -6,9 +6,9 @@ function view_normals_image(image)
     imshow(image_gray);
     
     count = tot_col * tot_row;
-	normals = zeros(tot_row, tot_col, 3);
+	%normals = zeros(tot_row, tot_col, 3);
    
-    
+    %{
     for i=1:1:tot_row
         for j=1:1:tot_col
             P = [i j];
@@ -33,7 +33,7 @@ function view_normals_image(image)
             end
             x = double(r/255);
             y = double(g/255);
-            z = (1-x^2-y^2);
+            z = sqrt(1-x^2-y^2);
             normals(i, j, 1) = x;
             normals(i, j, 2) = y;
             normals(i, j, 3) = z;
@@ -41,20 +41,20 @@ function view_normals_image(image)
             %normals = [normals; [x y z]];
         end
     end
-    
+    %}
     
     imageHandle = get(gca, 'Children');
     
     hSlider = uicontrol('Style', 'slider', 'Min', 1, 'Max', 400, 'Value', 51, 'Position', [400 20 220 20]);
     
-    change_callback_function = {@image_cick, imageHandle, hSlider, normals, image_gray, tot_row, tot_col};
+    change_callback_function = {@image_cick, imageHandle, hSlider, image, image_gray, tot_row, tot_col};
     
     set(hSlider, 'Callback', change_callback_function)
     set(imageHandle, 'ButtonDownFcn', change_callback_function);
     
 end
 
-function image_cick(~, ~, imageHandle, hSlider, normals, image_gray, tot_row, tot_col)
+function image_cick(~, ~, imageHandle, hSlider, image, image_gray, tot_row, tot_col)
     c_row = tot_row / 2;
     c_col = tot_col / 2;
     loc = get(gca, 'Currentpoint');
@@ -72,8 +72,31 @@ function image_cick(~, ~, imageHandle, hSlider, normals, image_gray, tot_row, to
         for j=1:1:tot_col
 			%index = 3*count;
 			%n = [normals(index+1) normals(index+2) normals(index+3)];
-	    	image_gray(i, j) = 255 * (light(1)*normals(i,j,1) + light(2)*normals(i,j,2) + light(3)*normals(i,j,3));
-        end
+	    	%image_gray(i, j) = 255 * (light(1)*normals(i,j,1) + light(2)*normals(i,j,2) + light(3)*normals(i,j,3));
+			r = double(image(i, j, 1));
+            g = double(image(i, j, 2));
+            b = image(i, j, 3);
+            if (b == 192)
+                %b
+                r = 0 - r;
+                g = g;
+            elseif (b == 128)
+                r = -r;
+                g = -g;
+            elseif (b == 64)
+                r = r; 
+                g = g;
+            else
+                %b
+                r = r;
+                g = 0 - g;
+            end
+            x = double(r/255);
+            y = double(g/255);
+            z = sqrt(1-x^2-y^2);   
+
+			image_gray(i, j) = 255 * (light(1)*x + light(2)*y + light(3)*z);
+		end
     end
     
     frame = image_gray;
